678caa3f64c0ccef5cf29b84da05beabc0d44b35
cdhash <cdresher2010@gmail.com>
2020-09-07 18:01:21 +0000

    completed thru 7

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -31,9 +31,9 @@ def calculate_demographic_data(print_data=True):
     min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df['hours-per-week'].loc[(df['hours-per-week'] == min_work_hours)].count()
 
-    rich_percentage = None
+    rich_percentage = num_min_workers/df['hours-per-week'].count() *100
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
